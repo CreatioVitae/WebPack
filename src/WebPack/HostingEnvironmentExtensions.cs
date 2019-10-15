30962464fe5228f6b00b742f4b143ea268be0e4f
h.chiba <h.chiba201103@gmail.com>
2019-10-15 14:30:00 +0900

    Edit HostingEnvironmentExtension => WebHostEnvironmentExtension;

--- src/WebPack/HostingEnvironmentExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Hosting;
-
-namespace WebPack {
-    public static class HostingEnvironmentExtensions {
-        public static bool IsDevelopmentRemote(this IHostingEnvironment hostingEnvironment) {
-            return hostingEnvironment.IsEnvironment(DefaultEnvironmentNames.DevelopmentRemote);
-        }
-    }
-}
---
+++ src/WebPack/WebHostEnvironmentExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace WebPack {
+    public static class WebHostEnvironmentExtensions {
+        public static bool IsDevelopmentRemote(this IWebHostEnvironment webHostEnvironment) {
+            return (webHostEnvironment.EnvironmentName == DefaultEnvironmentNames.DevelopmentRemote);
+        }
+    }
+}
