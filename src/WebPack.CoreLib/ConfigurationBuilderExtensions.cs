6d9312b8105d778decac9435f28ce550437fb54c
CreatioVitae <h.chiba201103@gmail.com>
2020-10-12 11:13:32 +0900

    Add ElasticSearch Formatter

--- src/WebPack.CoreLib/ConfigurationBuilderExtensions.cs
+++ src/WebPack.CoreLib/ConfigurationBuilderExtensions.cs
@@ -1,17 +1,16 @@
-﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting;
 using Serilog;
 using System.IO;
 
 namespace Microsoft.Extensions.Configuration {
     public static class ConfigurationBuilderExtensions {
         public static ILogger CreateDefaultLogger(this ConfigurationBuilder configurationBuilder) {
-            static IConfigurationRoot CreateDefaultConfigurationRoot(ConfigurationBuilder configurationBuilder) {
-                return configurationBuilder
+            static IConfigurationRoot CreateDefaultConfigurationRoot(ConfigurationBuilder configurationBuilder) =>
+                configurationBuilder
                     .SetBasePath(Directory.GetCurrentDirectory())
                     .AddJsonFile("appsettings.json")
                     .AddJsonFile($"appsettings.{DefaultEnvironment.GetEnvironmentName()}.json")
                     .Build();
-            }
 
             return new LoggerConfiguration().ReadFrom.Configuration(CreateDefaultConfigurationRoot(configurationBuilder)).CreateLogger();
         }
