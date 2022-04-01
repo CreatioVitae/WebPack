a369050afaf3012e654222ed350fdcea764b7873
CreatioVitae <h.chiba201103@gmail.com>
2022-04-01 22:12:14 +0900

    add : WebPack.GcpCoreLib

---
+++ src/WebPack.Gcp.CoreLib/WebHostBuilderExtensions.cs
@@ -0,0 +1,12 @@
+namespace Microsoft.AspNetCore.Hosting;
+
+public static class WebHostBuilderExtensions {
+    public static IWebHostBuilder UseCloudRun(this IWebHostBuilder webBuilder) {
+        static string GetCloudRunInternalUrl() =>
+            !int.TryParse(Environment.GetEnvironmentVariable("PORT"), out var port)
+                ? throw new InvalidCastException($"環境変数から取得した{nameof(port)}がintにキャスト出来ませんでした。")
+                : new UriBuilder(Uri.UriSchemeHttp, "0.0.0.0", port).ToString();
+
+        return webBuilder.UseUrls(GetCloudRunInternalUrl());
+    }
+}
