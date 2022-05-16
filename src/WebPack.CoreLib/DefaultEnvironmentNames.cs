a2648369871e8df68765244682a685a9afaef40d
CreatioVitae <h.chiba201103@gmail.com>
2022-05-16 20:43:03 +0900

    add IsDevelopment, IsDevelopmentRemote, IsStaging, IsProduction method...

--- src/WebPack.CoreLib/DefaultEnvironmentNames.cs
+++ src/WebPack.CoreLib/DefaultEnvironmentNames.cs
@@ -12,4 +12,16 @@ public static class DefaultEnvironmentNames {
 public static class DefaultEnvironment {
     public static string GetEnvironmentName() =>
         Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? DefaultEnvironmentNames.Production;
+
+    public static bool IsDevelopment() =>
+        GetEnvironmentName() is DefaultEnvironmentNames.Development;
+
+    public static bool IsDevelopmentRemote() =>
+        GetEnvironmentName() is DefaultEnvironmentNames.DevelopmentRemote;
+
+    public static bool IsStaging() =>
+        GetEnvironmentName() is DefaultEnvironmentNames.Staging;
+
+    public static bool IsProduction() =>
+        GetEnvironmentName() is DefaultEnvironmentNames.Production;
 }
