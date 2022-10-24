83d6022757a0cb6654d3aa49ef2475252bec41b9
CreatioVitae <h.chiba201103@gmail.com>
2022-10-24 15:22:28 +0900

    fix authentication enable.

--- src/WebPack.Identity.CoreLib/ServiceCollectionExtensions.cs
+++ src/WebPack.Identity.CoreLib/ServiceCollectionExtensions.cs
@@ -1,8 +1,15 @@
+using Microsoft.Extensions.Configuration;
 using WebPack.Identity.CoreLib;
 
+// ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
 public static class ServiceCollectionExtensions {
-    public static IServiceCollection AddBasicAuthenticationService<TBasicAuthenticationService>(this IServiceCollection services) where TBasicAuthenticationService : class, IBasicAuthenticationService {
+    public static IServiceCollection AddBasicAuthenticationService<TBasicAuthenticationService>(this IServiceCollection services, IConfiguration configuration) where TBasicAuthenticationService : class, IBasicAuthenticationService {
+        if (configuration.GetConfigureSettings() is { AuthenticationEnable: false }) {
+            return services;
+
+        }
+
         services.AddScoped<IBasicAuthenticationService, TBasicAuthenticationService>();
 
         services.AddAuthentication("BasicAuthentication").AddScheme<AuthenticationSchemeOptions, DefaultBasicAuthenticationHandler>("BasicAuthentication", null);
