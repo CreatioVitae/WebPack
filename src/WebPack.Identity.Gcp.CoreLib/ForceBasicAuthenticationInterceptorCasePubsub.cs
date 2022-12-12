9998ad81ac62d358733d509f906b57a1a7a713be
CreatioVitae <h.chiba201103@gmail.com>
2022-12-12 23:57:19 +0900

    repair querystring Reference.

--- src/WebPack.Identity.Gcp.CoreLib/ForceBasicAuthenticationInterceptorCasePubsub.cs
+++ src/WebPack.Identity.Gcp.CoreLib/ForceBasicAuthenticationInterceptorCasePubsub.cs
@@ -1,17 +1,27 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
+using System.Linq;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
+
 public class ForceBasicAuthenticationInterceptorCasePubSub : IBasicAuthenticationInterceptor {
     public static readonly string QueryKeyName = "force-basic-authentication-case-pub-sub-key";
 
-    public static (bool interruptResult, string? userName, string? password) Interrupt(HttpRequest httpRequest) =>
-        httpRequest.Query[QueryKeyName][0] is not string nonNull
-            ? (false, null, null)
-            : BasicAuthenticationExtensions.TryGetBasicAuthorizationCredentialsFromAuthenticationHeaderValueParameter(nonNull, out var userName, out var password)
+    public static (bool interruptResult, string? userName, string? password) Interrupt(HttpRequest httpRequest) {
+        if (httpRequest.Query.TryGetValue(QueryKeyName, out var queryVal) is false) {
+            return (false, null, null);
+        }
+
+        if (queryVal.FirstOrDefault() is not string nonNull) {
+            return (false, null, null);
+        }
+
+        return
+            BasicAuthenticationExtensions.TryGetBasicAuthorizationCredentialsFromAuthenticationHeaderValueParameter(nonNull, out var userName, out var password)
                 ? (true, userName, password)
                 : (false, null, null);
+    }
 
     public Func<HttpRequest, (bool interruptResult, string? userName, string? password)>? InterruptOnCredentials { get; } = Interrupt;
 }
