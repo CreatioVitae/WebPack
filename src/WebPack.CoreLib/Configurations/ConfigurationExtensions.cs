b635aee293bfb241536538f0bbc41f652e0f3a54
CreatioVitae <h.chiba201103@gmail.com>
2023-03-13 11:26:25 +0900

    edit ConfigureSettings.

--- src/WebPack.CoreLib/Configurations/ConfigurationExtensions.cs
+++ src/WebPack.CoreLib/Configurations/ConfigurationExtensions.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 
+// ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.Configuration;
 public static class ConfigurationExtensions {
     public static ConfigureSettings? GetConfigureSettings(this IConfiguration configuration) =>
--- src/WebPack.CoreLib/Configurations/ConfigureSettings.cs
+++ src/WebPack.CoreLib/Configurations/ConfigureSettings.cs
@@ -1,7 +1,10 @@
+// ReSharper disable once CheckNamespace
 namespace Microsoft.AspNetCore.Builder;
 
 public class ConfigureSettings {
     public bool AuthenticationEnable { get; init; }
 
     public PathBase? PathBase { get; init; }
+
+    public bool IgnoreForceRedirectsToHttps { get; init; } = false;
 }
