726285fa25c04b5477e10f013ac136419e74bf8e
hchiba1984 <h.chiba201103@gmail.com>
2023-08-05 18:29:51 +0900

    Add HttpPurgeAttribute.

---
+++ src/WebPack.CoreLib/HttpPurgeAttribute.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System.Net.Http;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.AspNetCore.Mvc;
+internal class HttpPurgeAttribute : HttpMethodAttribute {
+    private static readonly IEnumerable<string> SupportedMethods = new[] { ExtendedHttpMethod.PurgeMethodName };
+
+    /// <summary>
+    /// Creates a new <see cref="HttpGetAttribute"/>.
+    /// </summary>
+    public HttpPurgeAttribute()
+        : base(SupportedMethods) {
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="HttpGetAttribute"/> with the given route template.
+    /// </summary>
+    /// <param name="template">The route template. May not be null.</param>
+    public HttpPurgeAttribute([StringSyntax("Route")] string template)
+        : base(SupportedMethods, template) {
+        if (template == null) {
+            throw new ArgumentNullException(nameof(template));
+        }
+    }
+}
