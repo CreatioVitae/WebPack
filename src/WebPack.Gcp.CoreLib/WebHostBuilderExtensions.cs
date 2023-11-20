cebe3c268480fdf3de4cf39c38ce275cf9cc45f0
hchiba1984 <h.chiba201103@gmail.com>
2023-11-20 10:29:30 +0900

    Repair Adhoc Use Cloud Run. (#27)

--- src/WebPack.Gcp.CoreLib/WebHostBuilderExtensions.cs
+++ src/WebPack.Gcp.CoreLib/WebHostBuilderExtensions.cs
@@ -1,7 +1,10 @@
+// ReSharper disable once CheckNamespace
 namespace Microsoft.AspNetCore.Hosting;
 
 public static class WebHostBuilderExtensions {
     public static IWebHostBuilder UseCloudRun(this IWebHostBuilder webBuilder) {
+        AppContext.SetSwitch("System.Net.SocketsHttpHandler.Http3Support", false);
+
         static string GetCloudRunInternalUrl() =>
             !int.TryParse(Environment.GetEnvironmentVariable("PORT"), out var port)
                 ? throw new InvalidCastException($"環境変数から取得した{nameof(port)}がintにキャスト出来ませんでした。")
