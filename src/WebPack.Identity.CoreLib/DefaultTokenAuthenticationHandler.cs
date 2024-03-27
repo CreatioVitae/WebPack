c7f326ef10be0fffd89b551f7240bb4e5f9c3afa
hchiba1984 <h.chiba201103@gmail.com>
2024-03-27 14:38:02 +0900

    upgrade package 803. (#29)

--- src/WebPack.Identity.CoreLib/DefaultTokenAuthenticationHandler.cs
+++ src/WebPack.Identity.CoreLib/DefaultTokenAuthenticationHandler.cs
@@ -6,32 +6,25 @@ using System.Security.Claims;
 using System.Text.Encodings.Web;
 
 namespace WebPack.Identity.CoreLib;
-public class DefaultTokenAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions> {
-    readonly ITokenAuthenticationService _tokenAuthenticationService;
-    private readonly ITokenAuthenticationInterceptor _tokenAuthenticationInterceptor;
-
-    public DefaultTokenAuthenticationHandler(
-        IOptionsMonitor<AuthenticationSchemeOptions> options,
-        ILoggerFactory logger,
-        UrlEncoder encoder,
-        ISystemClock clock,
-        ITokenAuthenticationService tokenAuthenticationService,
-        ITokenAuthenticationInterceptor tokenAuthenticationInterceptor
-    ) : base(options, logger, encoder, clock) =>
-        (_tokenAuthenticationService, _tokenAuthenticationInterceptor) = (tokenAuthenticationService, tokenAuthenticationInterceptor);
-
+public class DefaultTokenAuthenticationHandler(
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder,
+    ITokenAuthenticationService tokenAuthenticationService,
+    ITokenAuthenticationInterceptor tokenAuthenticationInterceptor)
+    : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder) {
     protected override async Task<AuthenticateResult> HandleAuthenticateAsync() {
-        _tokenAuthenticationInterceptor.InterruptOnBeginnings?.Invoke(Request);
+        tokenAuthenticationInterceptor.InterruptOnBeginnings?.Invoke(Request);
 
         if (Context.GetEndpoint()?.Metadata?.GetMetadata<IAllowAnonymous>() is not null) {
             return AuthenticateResult.NoResult();
         }
 
-        if (Request.TryGetTokenAuthenticationCredential(out var token, _tokenAuthenticationInterceptor.InterruptOnCredentials) is false) {
+        if (Request.TryGetTokenAuthenticationCredential(out var token, tokenAuthenticationInterceptor.InterruptOnCredentials) is false) {
             return AuthenticateResult.Fail("Invalid Authorization Header");
         }
 
-        var (authenticateResult, claims) = await _tokenAuthenticationService.AuthenticateAsync(token);
+        var (authenticateResult, claims) = await tokenAuthenticationService.AuthenticateAsync(token);
 
         if (authenticateResult is false) {
             return AuthenticateResult.Fail($"Invalid {nameof(token)}.");
