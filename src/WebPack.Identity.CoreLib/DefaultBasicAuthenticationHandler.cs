9e6877076b00181e6b8865f23eacf46bf436c1b1
hchiba1984 <h.chiba201103@gmail.com>
2024-03-27 14:37:43 +0900

    upgrade package 803.

--- src/WebPack.Identity.CoreLib/DefaultBasicAuthenticationHandler.cs
+++ src/WebPack.Identity.CoreLib/DefaultBasicAuthenticationHandler.cs
@@ -6,30 +6,23 @@ using System.Security.Claims;
 using System.Text.Encodings.Web;
 
 namespace WebPack.Identity.CoreLib;
-public class DefaultBasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions> {
-    readonly IBasicAuthenticationService _basicAuthenticationService;
-    private readonly IBasicAuthenticationInterceptor _basicAuthenticationInterceptor;
-
-    public DefaultBasicAuthenticationHandler(
-        IOptionsMonitor<AuthenticationSchemeOptions> options,
-        ILoggerFactory logger,
-        UrlEncoder encoder,
-        ISystemClock clock,
-        IBasicAuthenticationService basicAuthenticationService,
-        IBasicAuthenticationInterceptor basicAuthenticationInterceptor
-    ) : base(options, logger, encoder, clock) =>
-        (_basicAuthenticationService, _basicAuthenticationInterceptor) = (basicAuthenticationService, basicAuthenticationInterceptor);
-
+public class DefaultBasicAuthenticationHandler(
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder,
+    IBasicAuthenticationService basicAuthenticationService,
+    IBasicAuthenticationInterceptor basicAuthenticationInterceptor)
+    : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder) {
     protected override async Task<AuthenticateResult> HandleAuthenticateAsync() {
         if (Context.GetEndpoint()?.Metadata?.GetMetadata<IAllowAnonymous>() is not null) {
             return AuthenticateResult.NoResult();
         }
 
-        if (Request.TryGetBasicAuthenticationCredential(out var userName, out var password, _basicAuthenticationInterceptor.InterruptOnCredentials) is false) {
+        if (Request.TryGetBasicAuthenticationCredential(out var userName, out var password, basicAuthenticationInterceptor.InterruptOnCredentials) is false) {
             return AuthenticateResult.Fail("Invalid Authorization Header");
         }
 
-        var (authenticateResult, claims) = await _basicAuthenticationService.AuthenticateAsync(userName, password);
+        var (authenticateResult, claims) = await basicAuthenticationService.AuthenticateAsync(userName, password);
 
         if (authenticateResult is false) {
             return AuthenticateResult.Fail("Invalid Username or Password");
